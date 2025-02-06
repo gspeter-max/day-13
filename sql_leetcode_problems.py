7a8ec53b1eab8e8ba3a0c7ee7e9e363725b3cde7
gspeter-max <gspeter67@gmail.com>
2025-02-06 11:41:21 +0530

    Update sql_leetcode_problems.py

--- sql_leetcode_problems.py
+++ sql_leetcode_problems.py
@@ -28,3 +28,44 @@ with temp_temp as (
 )
 select * 
 from temp_temp order by employee_id ; 
+
+
+''' Problem 1: Complex Fraud Detection with Recursive Analysis (Very Hard)
+Description
+You are given a table tracking financial transactions. 
+Fraudulent transactions are identified based on circular money movement—if a user sends money,
+and after a series of transactions, the money comes back to them, it’s flagged as fraud. 
+You must identify all users involved in circular transactions.
+''' 
+
+with recursive cycle_detection AS (
+	select sender_id  as start_user, 
+		receiver_id , 
+		amount, 
+		transaction_time, 
+		1 as depth , 
+		sender_id as cycle_path
+
+	from transactions 
+	where transaction_time between '2024-01-01 00:01:00' and  '2024-01-01 23:59:59' 
+
+	union all 
+
+	select 
+		c.start_user, 
+		t.receiver_id, 
+		t.amount, 
+		t.transaction_time, 
+		c.depth + 1, 
+		concat(c.cycle_path,'->',t.receiver_id) as cycle_path 
+
+	from cycle_detection c 
+	join transactions t  on c.receiver_id = t.sender_id 
+		and t.transaction_time between c.transaction_time and c.transaction_time + interval 24 hour
+	where c.depth < 5 
+	) 
+select distinct start_user as user_id 
+from cycle_detection 
+where start_user = receiver_id
+and depth >= 3 ; 
+
